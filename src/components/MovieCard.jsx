c5deeb67381f23755f4b273aa5b12e15c8eb75cf
Shravan250 <shravanbobade25@gmail.com>
2025-06-17 16:40:12 +0530

    making a proxy for fetch

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -20,10 +20,8 @@ const MovieCard = ({
             <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
 
-          <span>•</span>
           <p className="lang">{original_language}</p>
 
-          <span>•</span>
           <p className="year">
             {release_date ? release_date.split("-")[0] : "N/A"}
           </p>
